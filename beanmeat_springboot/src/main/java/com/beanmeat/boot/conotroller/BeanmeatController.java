ac115513f06b8ae60023d514281b5d3a99f5c7df
tianchenhui <3090018282@qq.com>
2025-06-16 13:05:13 +0800

    @PropertySource

--- beanmeat_springboot/src/main/java/com/beanmeat/boot/conotroller/BeanmeatController.java
+++ beanmeat_springboot/src/main/java/com/beanmeat/boot/conotroller/BeanmeatController.java
@@ -1,5 +1,9 @@
 package com.beanmeat.boot.conotroller;
 
+import com.beanmeat.boot.entity.Beanmeat;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("beanmeat")
 public class BeanmeatController {
 
+    @Resource
+    private Beanmeat beanmeat;
+
     @GetMapping("/index")
     public String beanmeat() {
         return "hello beanmeat";
+    }
+
+    @GetMapping("/getBeanmeat")
+    public String getBeanmeat() {
+        return beanmeat.toString();
     }
 }
